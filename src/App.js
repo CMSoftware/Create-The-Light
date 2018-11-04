7ce82981ab4c2148b819875b57de01d62b81b57b
vzt7 <vvzt666666@foxmail.com>
2018-11-04 21:58:31 +0800

    header finished. skrskr~

--- src/App.js
+++ src/App.js
@@ -14,20 +14,34 @@ import About from './components/body-about.jsx';
 import Footer from './components/footer.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navIndex: 0,
+    }
+  }
   render() {
     return (
       <div className="App">
         <Router>
           <div>
             <Header>
-                <ul>
-                  <li><Link to="/">首页</Link></li>
-                  <li><a href="//www.baidu.com">动态</a></li>
-                  <li><Link to="/members">成员</Link></li>
-                  <li><Link to="/about">关于</Link></li>
-                </ul>
+              <ul>
+                <li onClick={() => this.setState({ navIndex: 0 })} className={this.state.navIndex === 0 ? 'nav--active' : ''}>
+                  <Link to="/">首页</Link>
+                </li>
+                <li onClick={() => {}}>
+                  <a href="//www.baidu.com" target="_blank">空间</a>
+                </li>
+                <li onClick={() => this.setState({ navIndex: 2 })} className={this.state.navIndex === 2 ? 'nav--active' : ''}>
+                  <Link to="/members">成员</Link>
+                </li>
+                <li onClick={() => this.setState({ navIndex: 3 })} className={this.state.navIndex === 3 ? 'nav--active' : ''}>
+                  <Link to="/about">关于</Link>
+                </li>
+              </ul>
             </Header>
-            <Route path="/" component={Index}></Route>
+            <Route path="/" exact component={Index}></Route>
             <Route path="/members" component={Members}></Route>
             <Route path="/about" component={About}></Route>
           </div>
--- src/components/body-members.jsx
+++ src/components/body-members.jsx
@@ -1,7 +1,14 @@
+/*
+** Coding by vzt
+** 2018/11/4
+*/
 import React, { Component } from 'react';
 import './body-members.scss';
 
 export default class Members extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="members">Members</div>

25f29b818b5aa85fe231850a540fe725ad887c95
lsj <1210542555@qq.com>
2018-11-04 21:53:59 +0800

    fix：footer no.1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 /*
-** 首页底部栏
+** 首页底部栏jsx
 ** Coding by lsj
 ** 2018/11/4
 */
@@ -10,7 +10,29 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="">
+        <div className="footer__introduction">
+          <div className="footer__picture">
+            <img className="footer__picture--logo" src="https://webinput.nie.netease.com/img/my/icon.png" alt=""></img>
+            <div className="footer__picture--description">
+              备产劳段温在对该装部些听管价线与
+            </div>
+          </div>
+          <div className="footer__email">
+            chuangming2018skrskr@foxmail.com
+          </div>
+          <div className="footer__copyright">
+           Copyright © 2018 ChuangMing. All Rights Reserved
+          </div>
+        </div>
+        <div className="footer__link">
+          <div className="footer__link--title">友情链接</div>
+          <ul>
+            <li><a href="//www.baidu.com">Ivvna Huuubpcs Oclcjdgj Nscjyfef Tlrf Ixewowz</a></li>
+            <li><a href="//www.baidu.com">Dmzs Oprmudct Jubr Gehwp Lqxatjtrnn</a></li>
+            <li><a href="//www.baidu.com">Cgffbjkgw Drlsrkbube Uvppzptu Twzoml</a></li>
+            <li><a href="//www.baidu.com">Jrkewost Rltc Lwx Ysibkke Elscqzz Qeneim</a></li>
+            <li><a href="//www.baidu.com">Ivvna Huuubpcs Oclcjdgj Nscjyfef Tlrf Ixewowz</a></li>   
+          </ul>
         </div>
       </footer>
     );

668abf3c14eeb469f71fa09c45ee09e3dceeaec5
lsj <1210542555@qq.com>
2018-11-05 18:39:27 +0800

    members no.1

--- src/components/body-members.jsx
+++ src/components/body-members.jsx
@@ -1,17 +1,54 @@
 /*
-** Coding by vzt
-** 2018/11/4
+** Coding by lsj
+** 2018/11/5
 */
 import React, { Component } from 'react';
 import './body-members.scss';
 
+function Banner (props) {
+  return  <h3 id="bannerSwitch" className="members__banner--footer">{props.bannerText}</h3>;
+}
+
 export default class Members extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      arrText:[
+        "舔狗最终一无所有",
+        "你吼那么大声干什么",
+        "可以赢，但没必要"
+      ],
+      bannerText:'舔狗最终一无所有'
+    }
+  }
+  componentDidMount() {
+    let i = 0;
+    this.bannerTimer = setInterval(
+      () => {
+        // let bannerSwitch = document.querySelector('#bannerSwitch');
+        this.initialBanner(i);
+        i < this.state.arrText.length-1 ? i++ : i=0;
+      },
+      2000
+    );
+  }
+  initialBanner(i) {
+    const arrText = this.state.arrText;
+    // let bannerSwitch = document.querySelector('#bannerSwitch');
+    this.setState({bannerText: arrText[i]});
   }
   render() {
     return (
-      <div className="members">Members</div>
+      <div className="members">
+        <div className="members__banner">
+          <h1 className="members__banner--top">ChuangMing Members</h1>
+          <h1 className="members__banner--middle">创明工作室</h1>
+          <Banner bannerText={this.state.bannerText} />
+        </div>
+        <div className="members__section">
+          111
+        </div>
+      </div>
     );
   }
 }

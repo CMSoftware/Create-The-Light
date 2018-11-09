2d20288acdcb57b2d83e9303050b136506d50862
vzt7 <vvzt666666@foxmail.com>
2018-11-09 19:05:35 +0800

    api: set to cmsoft.github.io/create_the_light/*.json

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,8 @@
+const BASE = '//cmsoftware.github.io/create_the_light';
+
 export default {
-  base: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/base',
-  sections: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/sections',
-  carousel: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/carousel',
-  members: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/members'
+  base: `${BASE}/base.json`,
+  sections: `${BASE}/sections.json`,
+  carousel: `${BASE}/carousel.json`,
+  members: `${BASE}/members.json`
 }

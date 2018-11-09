d54212c4ddb9c6fb37017027ab9f5628d4841f63
vzt7 <vvzt666666@foxmail.com>
2018-11-09 17:11:12 +0800

    index-page: carousel bgsize fix

--- src/components/carousel-index.jsx
+++ src/components/carousel-index.jsx
@@ -71,7 +71,7 @@ export default class Carousel extends Component {
           {this.state.data && Array.isArray(this.state.data) ? this.state.data.map((item, index) => {
             return (
               <div key={index} className="swiper-slide">
-                <a href={item.url}><img src={item.image} /></a>
+                <a href={item.url}><div style={{ backgroundImage: `url(${item.image})` }}></div></a>
               </div>
             );
           }) : <Loading></Loading>}

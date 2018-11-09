9fe006ea131380158418c2f527074ed3af47a08d
lsj <1210542555@qq.com>
2018-11-09 22:39:44 +0800

    about skr skr

--- src/Api.js
+++ src/Api.js
@@ -2,5 +2,6 @@ export default {
   base: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/base',
   sections: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/sections',
   carousel: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/carousel',
-  members: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/members'
+  members: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/members',
+  about: 'https://www.easy-mock.com/mock/5bdd471b2648a72a8d8a71f2/cm/about'
 }
